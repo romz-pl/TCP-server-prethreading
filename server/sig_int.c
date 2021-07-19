65e35b4c8d6759a2c855f1784d37505577b0613f
romz <romz@wp.pl>
2021-07-19 17:49:28 +0200

    Add definition of function sig_int

--- server/sig_int.c
+++ server/sig_int.c
@@ -1,2 +1,22 @@
 #include "sig_int.h"
 
+#include "nthreads.h"
+#include "pr_cpu_time.h"
+#include "tptr.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+
+void
+sig_int(int signo)
+{
+	int		i;
+
+	pr_cpu_time();
+
+	for (i = 0; i < nthreads; i++)
+		printf("thread %d, %ld connections\n", i, tptr[i].thread_count);
+
+	exit(0);
+}
+
