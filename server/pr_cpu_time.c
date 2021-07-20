da25c57d166d6475ed3a92fe1f9ce9f5a17caddf
romz <romz@wp.pl>
2021-07-20 08:00:16 +0200

    Add definition of function pr_cpu_time

--- server/pr_cpu_time.c
+++ server/pr_cpu_time.c
@@ -1,2 +1,29 @@
 #include "pr_cpu_time.h"
 
+#include "err_quit.h"
+
+#include <sys/resource.h>
+#include <stdio.h>
+
+void pr_cpu_time(void)
+{
+	double			user, sys;
+	struct rusage	myusage, childusage;
+
+	if (getrusage(RUSAGE_SELF, &myusage) < 0)
+		err_quit("getrusage error");
+	if (getrusage(RUSAGE_CHILDREN, &childusage) < 0)
+		err_quit("getrusage error");
+
+	user = (double) myusage.ru_utime.tv_sec +
+					myusage.ru_utime.tv_usec/1000000.0;
+	user += (double) childusage.ru_utime.tv_sec +
+					 childusage.ru_utime.tv_usec/1000000.0;
+	sys = (double) myusage.ru_stime.tv_sec +
+				   myusage.ru_stime.tv_usec/1000000.0;
+	sys += (double) childusage.ru_stime.tv_sec +
+					childusage.ru_stime.tv_usec/1000000.0;
+
+	printf("\nuser time = %g, sys time = %g\n", user, sys);
+}
+
